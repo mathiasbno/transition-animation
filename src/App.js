c0172fe6dcb2b05b2ca547998a6e272930018ec4
Mathias Benjaminsen <mabenjam@gmail.com>
2019-09-06 22:40:56 +0200

    Add router and transition component to app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// ./src/App.js
+import { Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Article from './pages/Article';
+import FrontPage from './pages/FrontPage';
+
+class App extends Component {
+  render() {
+    return (
+      <Route render={({ location }) => (
+        <TransitionGroup component={null}>
+          <CSSTransition
+            key={location.key}
+            appear={true}
+            timeout={{ enter: 1000, exit: 1000 }}
+          >
+            <Switch location={location}>
+              <Route key="/" path="/" exact component={FrontPage}></Route>
+              <Route key="/article" path="/article" exact component={Article}></Route>
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
+      )} />
+    );
+  }
 }
 
 export default App;
